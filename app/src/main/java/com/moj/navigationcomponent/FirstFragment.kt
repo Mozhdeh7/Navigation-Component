c6c3f92d69d86b4e29358bdd7c9fd3bcc5253206
mozhdeh7 <mnajafi93@gmail.com>
2023-01-08 21:04:39 +0330

    create navigation host and destinations

---
+++ app/src/main/java/com/moj/navigationcomponent/FirstFragment.kt
@@ -0,0 +1,35 @@
+package com.moj.navigationcomponent
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.buttonGotoSecond
+
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonGotoSecond.setOnClickListener {
+         //  Navigation.findNavController(it).navigate(FirstFragmentDirections.)
+        }
+
+    }
+}
